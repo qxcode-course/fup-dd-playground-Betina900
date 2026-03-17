ecf1a611276d505b6b9fb826ece10c3a1b563035
Betina900 <betina.silva@alu.ufc.br>
2026-03-17 14:29:41 +0000

    commit.07

--- fup/celsius/go/draft.go
+++ fup/celsius/go/draft.go
@@ -1,5 +1,9 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("qxcode")
+    var temp_celsius float64
+    fmt.Scan(&temp_celsius)
+
+    var temp_fahrenheit = (1.8*temp_celsius)+32
+    fmt.Printf("%.6f\n", temp_fahrenheit)
 }
