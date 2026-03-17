8493598399be94ab654f8d7693d47ecf7a73d20e
Betina900 <betina.silva@alu.ufc.br>
2026-03-17 14:18:43 +0000

    commit.06

--- fup/segundos/go/draft.go
+++ fup/segundos/go/draft.go
@@ -5,6 +5,7 @@ func main() {
     fmt.Scan(&tempo)
     var hora = tempo/3600
     var minutos = (tempo%3600)/60
-    var segundo = ((tempo%3600)/60)/60
-    fmt.Printf("%d:%d:%d\n", hora, minutos,segundo)
+    var segundo = tempo%60
+
+    fmt.Printf("%d:%d:%d\n", hora, minutos, segundo)
 }
