55bdb61de6d74db038f26b264b03d2215a6483bc
Betina900 <betina.silva@alu.ufc.br>
2026-03-18 13:44:01 +0000

    questao abs

--- fup/bala/go/draft.go
+++ fup/bala/go/draft.go
@@ -1,5 +1,13 @@
 package main
-import "fmt"
+import (
+    "fmt"
+    "math"
+)
 func main() {
-    fmt.Println("qxcode")
+    var x1, y1, x2, y2 float64
+    fmt.Scan(&x1, &y1, &x2, &y2)
+
+    var dab = ((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1))
+    var raiz = math.Sqrt(dab)
+    fmt.Printf("%.2f\n", raiz)
 }
