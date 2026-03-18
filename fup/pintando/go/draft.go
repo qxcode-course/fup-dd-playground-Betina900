056ca05825a30c3025a3467381f3a2b5ac73ee9c
Betina900 <betina.silva@alu.ufc.br>
2026-03-18 01:08:06 +0000

    questao pintando

--- fup/pintando/go/draft.go
+++ fup/pintando/go/draft.go
@@ -1,5 +1,14 @@
 package main
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 func main() {
-    
+    var a, b, c float64
+	fmt.Scan(&a, &b, &c)
+
+	var p = (a+b+c)/2
+	var area = ((p-a)*(p-b)*(p-c))*p
+	var raiz = math.Sqrt(area)
+	fmt.Printf("%.2f\n", raiz)
 }
