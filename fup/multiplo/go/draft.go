e70655ad4aa858be3efc7f9c7710dfd5fcd80ad7
Betina900 <betina.silva@alu.ufc.br>
2026-03-18 17:47:17 +0000

    questoes positivo e multiplo

---
+++ fup/multiplo/go/draft.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+func main() {
+    var numero int
+    fmt.Scan(&numero)
+
+    if (numero%7==0) {
+        fmt.Println("SIM")
+    } else {
+        fmt.Println("NAO")
+    }
+}
---
+++ fup/positivo/go/draft.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+func main() {
+    var numero int
+    fmt.Scan(&numero)
+
+    if numero >= 0 {
+        fmt.Println("SIM")
+    } else {
+        fmt.Println("NAO")
+    }
+}
