cf146e45f2db96c340bdd3a8d35d8e4d3c011c6d
Betina900 <betina.silva@alu.ufc.br>
2026-03-23 17:56:28 +0000

    questao guarda

--- fup/guarda/go/draft.go
+++ fup/guarda/go/draft.go
@@ -1,21 +1,16 @@
  package main
 import "fmt"
 func main() {
-    var wifi, login, adm bool
+    var wifi, login, adm int
     fmt.Scan(&wifi, &login, &adm)
 
-    1 := true
-    0 := false
-    switch {
-    case wifi==0:
+    if wifi==0 {
         fmt.Println("you must connect to wifi")
-    case login==0:
+    } else if login==0 {
         fmt.Println("you need to login first")
-    case adm==0:
-        fmt.Println("you must login as admin")
-    case wifi==1 && login==1 && adm==1:
+    } else if adm==0 {
+        fmt.Println("you must to login as admin")
+    } else {
         fmt.Println("done")
-    default:
-        fmt.Println("ERRO")
     }
 }
