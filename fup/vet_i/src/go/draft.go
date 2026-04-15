bf3bb1c8d5f212e086e374903a9ced48b080ac76
Betina900 <betina.silva@alu.ufc.br>
2026-04-15 14:46:23 +0000

    questao vet_i

--- fup/vet_i/src/go/draft.go
+++ fup/vet_i/src/go/draft.go
@@ -3,8 +3,19 @@ import "fmt"
 func main() {
     var elementos int
     fmt.Scan(&elementos)
-    var arr []int = make([]int, elementos)
-    for i:=0; i<len(arr); i++{
+
+    arr := make([]int, elementos)
+
+    if elementos == 0 {
+        fmt.Println()
+        return
+    }
+    
+    for i := 0; i < elementos; i++ {
+        fmt.Scan(&arr[i])
+    }
+
+    for i := 0; i < elementos; i++ {
         fmt.Println(arr[i])
     }
 }
