7b69d97e1174e434f818fe2cd5c758ad9957afd3
Betina900 <betina.silva@alu.ufc.br>
2026-05-04 18:16:34 +0000

    quest. apertada e game10

--- fup/apertada/src/go/draft.go
+++ fup/apertada/src/go/draft.go
@@ -1,5 +1,14 @@
 package main
 import "fmt"
 func main() {
-    var valores []int := make([]int,0, 30)
+    var valores int
+    fmt.Scan(&valores)
+    nums := make([]int, valores) //criando lista
+
+    for i:=0; i<valores; i++{ //lendo valores
+        fmt.Scan(&nums[i])
+    }
+    menor := nums[0] //o menor número
+
+    fmt.Println(menor)
 }
--- fup/game10/src/go/draft.go
+++ fup/game10/src/go/draft.go
@@ -3,4 +3,6 @@ import "fmt"
 func main() {
     var n_posicoes, disco, aviao int
     fmt.Scan(&n_posicoes, &disco, &aviao)
+    distancia := (disco-aviao+n_posicoes)%n_posicoes
+    fmt.Println(distancia)
 }
